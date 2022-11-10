12424686d15615aec89ab2b9bcb8f6c60c4e7d5a
Jonah <jonahcole@yahoo.com>
2022-11-10 11:40:15 -0800

    Add form with input validation instead of only basic inputs

--- Data/Grocery.cs
+++ Data/Grocery.cs
@@ -1,8 +1,21 @@
-﻿namespace LearnBlazorShoppingList.Data
+﻿//this is for form validation
+using System.ComponentModel.DataAnnotations;
+
+
+namespace LearnBlazorShoppingList.Data
 {
     public class Grocery
     {
+        // This was all we had/needed before form validation
+        //public string Name { get; set; }
+        //public double Price { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 1, ErrorMessage = "Name should be less than 30 characters.")]
         public string Name { get; set; }
+
+        [Required]
+        [Range(.01, 100000, ErrorMessage = "Valid Price Range is $.01 to $100,000")]
         public double Price { get; set; }
 
     }
